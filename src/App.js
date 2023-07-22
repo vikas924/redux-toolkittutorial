f800c5d26419d2b4e2b9389505347cbdbafb29a9
vikas924 <obroy902@gmail.com>
2023-07-22 21:07:10 +0530

    exercise

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
+import Modal from './components/Modal';
+
+function App() {
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, [dispatch]);
+  const { isOpen } = useSelector((state) => state.modal);
+
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+
+export default App;
